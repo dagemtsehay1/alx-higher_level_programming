b32913199b147cb45fffadd3fd95ff134a80316e
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-09-07 14:33:16 +0300

    remove char at

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    part = str[:n]
-    parts = str[n+1:]
-    return part + parts
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
