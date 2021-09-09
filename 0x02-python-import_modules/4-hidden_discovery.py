2431694a60e8b6433a6cba94fe2878f44559d4ab
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-09-09 12:07:07 +0300

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-
 if __name__ == "__main__":
-    file = dir(hidden_4)
-    length = len(file)
-    for i in range(0, length):
-        if file[i][0:2] != "__":
-            print(file[i])
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
