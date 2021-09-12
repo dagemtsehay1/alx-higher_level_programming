20515bcc606cc0edc60c72ce0ffeb0b80a49b80d
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-09-12 10:56:14 +0300

    data structure

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-def element_at(my_list,idx):
-    if idx < 0 or idx > (len(my_list) -1):
+def element_at(my_list, idx):
+    if (len(my_list) - 1) < idx or idx < 0:
         return None
-    return (my_list[idx])
+    else:
+        return my_list[idx]
