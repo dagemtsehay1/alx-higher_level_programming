0a5afb0b6ce1291d60e4638e26a84a9696b079d4
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-09-12 11:22:10 +0300

    data structure

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
+    return ''.join(c for c in my_string if c not in 'cC')
