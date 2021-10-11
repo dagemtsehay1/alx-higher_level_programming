175d810c1f9435c41d2dcd5f59436c93764e51eb
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-10-11 12:12:17 +0300

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines an object attribute lookup function."""
+
+
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
+
+    © 2021 GitHub, Inc.
+    Terms
+    Privacy
