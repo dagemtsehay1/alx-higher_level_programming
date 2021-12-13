8b64f4365e887b92ddda42d547770aa31844dd00
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-12-13 17:20:00 +0300

    adding new line

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const myStrings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < myStrings.length; i++) {
+  console.log(myStrings[i]);
 }
