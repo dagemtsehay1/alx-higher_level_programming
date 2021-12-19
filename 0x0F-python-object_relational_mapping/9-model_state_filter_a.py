3208b725e76df652ef73df5ea308f27eb23d977d
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-12-19 12:19:38 +0300

    task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-# Lists all State objects that contain the letter a
+'''script for task 9'''
 
-import sys
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+import sys
 
-if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
+    local_session = Session()
+    a_states = local_session.query(State).filter(
+               State.name.op('regexp')('.*a+.*')
+               ).order_by(State.id)
+    local_session.close()
+    engine.dispose()
 
-    for state in session.query(State).order_by(State.id):
-        if "a" in state.name:
-            print("{}: {}".format(state.id, state.name))
+    if a_states:
+        for state in a_states:
+            print('{}: {}'.format(state.id, state.name))
