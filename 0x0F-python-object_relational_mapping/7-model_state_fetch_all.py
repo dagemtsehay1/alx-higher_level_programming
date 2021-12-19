9d03994c1ca258dccc3c0fdcd9879629e69e99b4
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-12-19 12:18:06 +0300

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-# Lists all State objects from the database hbtn_0e_6_usa.
+'''script  for task 7'''
 
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+import sys
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+             username, password, host, port, db_name), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    session = Session()
+    local_session = Session()
+    states = local_session.query(State).order_by(State.id).all()
+    local_session.close()
+    engine.dispose()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+    for state in states:
+        print(str(state.id) + ': ' + state.name)
