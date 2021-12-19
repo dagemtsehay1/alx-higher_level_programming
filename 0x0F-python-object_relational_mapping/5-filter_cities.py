8d03a80a8cfcc97e58830bd25c5ca23807eb6ba5
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2021-12-19 12:15:28 +0300

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
+'''script for task 5'''
 
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+if __name__ == '__main__':
+    '''lists all cities of a state passed as argument to the script'''
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+    host = 'localhost'
+    port = 3306
+
+    db = MySQLdb.connect(host=host, user=username, passwd=password,
+                         db=db_name, port=port)
+    cur = db.cursor()
+    cur.execute('SELECT c.name FROM cities c INNER JOIN states s ' +
+                'ON s.id = c.state_id WHERE ' +
+                'BINARY s.name = %s ' +
+                'ORDER BY c.id ASC;', [state_name])
+    result = cur.fetchall()
+    cur.close()
+    db.close()
+
+    print(', '.join(map(lambda x: x[0], result)))
