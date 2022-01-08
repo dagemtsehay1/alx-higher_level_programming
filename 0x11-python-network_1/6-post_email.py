a988fb90fc1d82e4de5fa73a6fb4be9cf4fa1fa6
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2022-01-08 15:28:50 +0300

    task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email."""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
