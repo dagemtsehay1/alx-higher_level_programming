4132beb587781b24be0b3b646fd0cfd5c18f91e4
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2022-02-01 17:08:35 +0300

    hot fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+
+if (process.argv.length > 2) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  request(url, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const movie = JSON.parse(body);
+      movie.characters.map(char => {
+        request(char, (err, res, body) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(JSON.parse(body).name);
+          }
+        });
+        return (char);
       });
-    });
-  }
-});
+    }
+  });
+}
