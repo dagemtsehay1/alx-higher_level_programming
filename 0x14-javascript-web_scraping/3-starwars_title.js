d340f4931386b31b5ccd1f060ad196083a90b483
Dagem Tsehay <84669128+dagemtsehay1@users.noreply.github.com>
2022-02-01 14:13:53 +0300

    hotfix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+if (process.argv.length > 2) {
+  const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+  request(url, (err, res, body) => {
+    if (err) console.log(err);
+
+    console.log(JSON.parse(body).title);
+  });
+}
